1ac0004ce513e163e358f68f3de5043f64d3a185
Alex Milowski <alex@milowski.com>
2020-04-23 09:33:21 -0700

    Added version to init Fixed package naming typo ... may regret this name.

--- pygobo/__init__.py
+++ pygobo/__init__.py
@@ -3,3 +3,5 @@ from .cypher import query_generate, query_create_ontology, query_create_term, qu
 
 __all__ = ['Ontology', 'OBOParser',
            'query_generate', 'query_create_ontology', 'query_create_term', 'query_cross_reference_term', 'query_create_typedef']
+
+__version__ = '0.1.0'
